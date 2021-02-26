da4b9b84c2874c3ed472f71dd6f9563c7939ad23
Cornelius <bcornelius@iseatz.com>
2021-02-26 10:27:37 -0600

    creates filter function

--- src/app/media/services/videos.service.ts
+++ src/app/media/services/videos.service.ts
@@ -15,4 +15,35 @@ export class VideosService {
   filteredVideos: IVideo[];
 
   clickVideo: Function = video => this.activeVideo = video;
+  hasDuplicate: Function = (array, value) => {
+    for( let i=0; i<array.length; i++) {
+      if(array[i].title === value.title && array[i].num === value.num) {
+        return true;
+      };
+    };
+    return false;
+  }
+  filterVideos: Function = (allVideos, criteriaKey, criteriaValue) => {
+    let result = [];
+    allVideos.reduce((agg, curr) => {
+      if(!this.hasDuplicate(agg, curr) && curr[criteriaKey] === criteriaValue) {
+        agg.push(curr);
+      };
+      return agg;
+    }, result)
+  }
+  multiFilterVideos: Function = (allVideos, criterionArray) => {
+    let results = [];
+    criterionArray.forEach(criteria => {
+      const key = Object.keys(criteria)[0];
+      const value = criteria[key];
+      const filteredResults = this.filterVideos(allVideos, key, value);
+      if(results.length === 0) {
+        results = filteredResults;
+      } else {
+        results.concat(filteredResults);
+      };
+    });
+    return results;
+  }
 }
