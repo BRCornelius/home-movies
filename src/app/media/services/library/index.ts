b12c3a7d50c5318a00e5115752acc5d097363b97
Cornelius <bcornelius@iseatz.com>
2021-03-09 16:43:15 -0600

    updates library

--- src/app/media/services/library/index.ts
+++ src/app/media/services/library/index.ts
@@ -1,8 +1,11 @@
 import unknownVideos from './unknown';
 import fifties from './fifties';
 import eighties from './eighties';
-import nineties from './nineties';
+import nineties01 from './nineties-01';
+import nineties02 from './nineties-02';
 
 const conjoinArrays = (arrayOfArrays) => arrayOfArrays.flat();
 
-export const data = conjoinArrays([unknownVideos, fifties, eighties, nineties]);
+export const data = conjoinArrays([
+    unknownVideos, fifties, eighties, nineties01, nineties02
+]);
