c31f838ff5051a8d1b2d89eb8f86110d6707e6f5
Cornelius <bcornelius@iseatz.com>
2021-03-11 07:39:43 -0600

    adds to library

--- src/app/media/services/library/index.ts
+++ src/app/media/services/library/index.ts
@@ -3,7 +3,8 @@ import fifties from './fifties';
 import eighties from './eighties';
 import nineties01 from './nineties-01';
 import nineties02 from './nineties-02';
+import nineties03 from './nineties-03';
 
-const nineties = [...nineties01, ...nineties02];
+const nineties = [...nineties01, ...nineties02, ...nineties03];
 
 export const data = [...unknownVideos, ...fifties, ...eighties, ...nineties];
