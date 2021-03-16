56e7d2c725764b0426433e5b76361017f7211946
Cornelius <bcornelius@iseatz.com>
2021-03-15 23:45:50 -0500

    adds year badge to media card

--- src/app/media/components/media-card/media-card.component.ts
+++ src/app/media/components/media-card/media-card.component.ts
@@ -1,14 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'media-card',
   templateUrl: './media-card.component.html',
   styleUrls: ['./media-card.component.css']
 })
-export class MediaCardComponent {
+export class MediaCardComponent implements OnInit{
 
   constructor() { }
 
+  ngOnInit() {
+    this.displayYear = this.year === 'unknown' ? '????' : `19${this.year}`
+  
+  }
+
   @Input() title: string;
   @Input() thumbnailUrl: string;
+  @Input() year: string;
+
+  displayYear: string;
 }
